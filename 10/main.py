9c15a0348bc21bc39e4c3911dbc816299094454f
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-12-10 09:04:27 -0700

    now with minvariation

--- 10/main.py
+++ 10/main.py
@@ -1,6 +1,5 @@
 import re
 import sys
-from collections import defaultdict
 
 r = re.compile(r'position=< *(-?[0-9]+), *(-?[0-9]+)> velocity=< *(-?[0-9]+), *(-?[0-9]+)>')
 def parse_point(v):
@@ -22,7 +21,6 @@ def print_board(l):
     max_x = max(v[0] for v in l)
     max_y = max(v[1] for v in l)
     b = []
-    print(min_x, max_x, min_y, max_y)
     for i in range(max_y - min_y + 1):
         b.append(['.'] * (max_x - min_x + 1))
     for p in l:
@@ -32,22 +30,37 @@ def print_board(l):
 
 def min_area(l):
     current_area = board_area(state_at(l, 0))
-    print(0, current_area)
     t = 1
     next_board = state_at(l, t)
     next_area = board_area(next_board)
-    print(t, next_area)
     while next_area < current_area:
         current_area = next_area
         t += 1
         next_board = state_at(l, t)
         next_area = board_area(next_board)
-        print("\r                             \r%s %s" % (t, next_area), end='')
+        if t % 1000 == 0:
+            print("\r                             \r%s %s" % (t, next_area), end='')
     print()
-    return t
+    return t-1
+
+def min_variation(l):
+    n = len(l)
+    a = sum(v[0] * v[2] for v in l)
+    b = sum(v[2] * v[2] for v in l)
+    c = sum(v[0] for v in l)
+    d = sum(v[2] for v in l)
+    e = sum(v[1] * v[3] for v in l)
+    f = sum(v[3] * v[3] for v in l)
+    g = sum(v[1] for v in l)
+    h = sum(v[3] for v in l)
+    v1 = (n * (n-1) * b - n * d * d) + (n * (n-1) * f - n * h * h )
+    v2 = (n * (n-1) * a - (n-1) * c * d) + (n * (n-1) * e - (n-1) * g * h)
+    return(-v2/v1)
 
 if __name__ == '__main__':
     l = list(parse_point(v) for v in sys.stdin.readlines())
+    print(round(min_variation(l)))
+    print(min_area(l))
     print_board(state_at(l, 10086))
     # print("min area: %s", min_area(l))
     # exit(1)
