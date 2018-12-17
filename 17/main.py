a58092944b1e7157542e956d736e4dba4d546bb4
Carlos Scheidegger <cscheid@cs.arizona.edu>
2018-12-17 14:54:48 -0700

    17

--- 17/main.py
+++ 17/main.py
@@ -1,7 +1,7 @@
-# 10:35AM
-
+# 10:35
+#   14:50
+#   14:52
 import sys
-import time
 
 f = sys.stdin.readlines()
 
@@ -21,9 +21,17 @@ def left_of(p):
     return (p[0]-1, p[1])
 def right_of(p):
     return (p[0]+1, p[1])
+def left(p):
+    return (p[0]-1, p[1])
+def right(p):
+    return (p[0]+1, p[1])
 def under(p):
     return (p[0], p[1]+1)
 def over(p):
+    return (p[0], p[1]-1)
+def below(p):
+    return (p[0], p[1]+1)
+def above(p):
     return (p[0], p[1]-1)
 
 def find_bounds(lines):
@@ -48,7 +56,6 @@ class Board:
         ((min_x, min_y), (max_x, max_y)) = bounds
         print("bounds: ", ((min_x, min_y), (max_x, max_y)))
         self.bounds = bounds
-        # x sentinels
         board = []
         for y in range(min_y, max_y+1):
             board.append(["."] * (max_x - min_x + 1))
@@ -79,71 +86,96 @@ class Board:
         for (x, y) in self.enumerate_squares():
             if self.board[y][x] in ['|', '~']:
                 yield (x, y)
+    def find_static_water(self):
+        for (x, y) in self.enumerate_squares():
+            if self.board[y][x] in ['~']:
+                yield (x, y)
+    def set_square_at(self, p, v):
+        self.board[p[1]][p[0]] = v
     def square_at(self, p):
-        return self.board[p[1]][p[0]]
+        try:
+            return self.board[p[1]][p[0]]
+        except IndexError:
+            return None
     def raycast(self, origin, d, cond=lambda t: True):
         p = origin
         try:
-            while self.square_at(p) == '.' and cond(p):
+            while cond(p):
                 p = (p[0] + d[0], p[1] + d[1])
         except IndexError:
             return None
         return p
-    def pour_from(self, p, indent=0):
-        global total_water
-        # print(" " * indent, "will pour from", p)
-        # raycast from spring to first clay or pooled water
-        p = under(p)
-        p = self.raycast(p, (0, 1))
-        if p is None:
-            # print("miss! at ", p)
-            return False
-        # print("hit! %s at %s" % (self.square_at(p), p))
-        water_hit = (p[0], p[1]-1)
-        if self.square_at(water_hit) != '.':
-            return False
-        left_boundary = self.raycast(water_hit, (-1, 0))
-        if left_boundary is None:
-            left_boundary = (0, water_hit[1])
-        right_boundary = self.raycast(water_hit, (1, 0))
-        if right_boundary is None:
-            right_boundary = (self.bounds[1][0], water_hit[1])
-        if all(self.square_at(under(ray_p)) in ['#', '~']
-               for ray_p in self.enumerate_range(right_of(left_boundary), left_of(right_boundary))):
-            # print("will fill at %s %s %s" % (water_hit, left_boundary[0]+1, right_boundary[0]))
-            for x in range(left_boundary[0]+1, right_boundary[0]):
-                self.board[left_boundary[1]][x] = '~'
-                total_water += 1 
-            return True
-        else:
-            result = False
-            # print("Can't fill, will pour from leaks")
-            p = self.raycast(water_hit, (-1, 0), lambda p: self.square_at(under(p)) in ['#', '~'])
-            if not (p is None) and self.square_at(p) == '.':
-                result = result or self.pour_from(p, indent+2)
-            p = self.raycast(water_hit, (1, 0), lambda p: self.square_at(under(p)) in ['#', '~'])
-            if not (p is None) and self.square_at(p) == '.':
-                result = result or self.pour_from(p, indent+2)
-            return result
-    def tick(self):
-        return self.pour_from(self.spring_location)
+    
+    def check_for_static_water(self, p):
+        if self.square_at(p) == '#':
+            r = self.raycast(right(p), (1, 0), lambda p: self.square_at(p) == '|' and self.square_at(below(p)) in ['~', '#'])
+            if r and self.square_at(r) == '#':
+                for x in range(p[0]+1, r[0]):
+                    self.set_square_at((x, p[1]), '~')
+                    self.open_squares.add((x, p[1]-1))
+            l = self.raycast(left(p), (-1, 0), lambda p: self.square_at(p) == '|' and self.square_at(below(p)) in ['~', '#'])
+            if l and self.square_at(l) == '#':
+                for x in range(l[0]+1, p[0]):
+                    self.set_square_at((x, p[1]), '~')
+                    self.open_squares.add((x, p[1]-1))
+        elif self.square_at(p) == '|' and self.square_at(below(p)) in ['~', '#', '|']:
+            l = self.raycast(left(p), (-1, 0), lambda p: self.square_at(p) == '|' and self.square_at(below(p)) in ['~', '#'])
+            r = self.raycast(right(p), (1, 0), lambda p: self.square_at(p) == '|' and self.square_at(below(p)) in ['~', '#'])
+            if l and r and self.square_at(l) == '#' and self.square_at(r) == '#':
+                for x in range(l[0]+1, r[0]):
+                    self.set_square_at((x, p[1]), '~')
+                    self.open_squares.add((x, p[1]-1))
+
+    def handle_square(self, p):
+        if self.square_at(p) == '.' and (self.square_at(above(p)) in ['|', '+']):
+            self.set_square_at(p, '|')
+            self.check_for_static_water(p)
+            self.open_squares.add(below(p))
+            self.open_squares.add(p)
+            if self.square_at(left(p)) == '.' and self.square_at(below(p)) in ['~', '#'] and self.square_at(below(left(p))) in ['~', '#']:
+                self.open_squares.add(left(p))
+            if self.square_at(right(p)) == '.' and self.square_at(below(p)) in ['~', '#'] and self.square_at(below(right(p))) in ['~', '#']:
+                self.open_squares.add(right(p))
+        elif self.square_at(p) == '.' and (self.square_at(right(p)) == '|') and (self.square_at(below(p)) in ['~', '|', '#']):
+            self.set_square_at(p, '|')
+            self.check_for_static_water(p)
+            self.open_squares.add(left(p))
+            self.open_squares.add(below(p))
+            self.open_squares.add(p)
+        elif self.square_at(p) == '.' and (self.square_at(left(p)) == '|') and (self.square_at(below(p)) in ['~', '|', '#']):
+            self.set_square_at(p, '|')
+            self.check_for_static_water(p)
+            self.open_squares.add(right(p))
+            self.open_squares.add(below(p))
+            self.open_squares.add(p)
+        elif self.square_at(p) == '#':
+            self.check_for_static_water(p)
+        elif self.square_at(p) == '|':
+            self.open_squares.add(below(p))
+            self.check_for_static_water(p)
+            if self.square_at(left(p)) == '.' and self.square_at(below(p)) in ['~', '#']:
+                self.set_square_at(left(p), '|')
+                self.open_squares.add(left(p))
+                self.open_squares.add(below(left(p)))
+                self.check_for_static_water(left(p))
+            if self.square_at(right(p)) == '.' and self.square_at(below(p)) in ['~', '#']:
+                self.set_square_at(right(p), '|')
+                self.open_squares.add(right(p))
+                self.open_squares.add(below(right(p)))
+                self.check_for_static_water(right(p))
             
+    def run(self):
+        self.open_squares = set()
+        self.open_squares.add(below(self.spring_location))
+        while len(self.open_squares):
+            self.handle_square(self.open_squares.pop())
         
 lines = list(parse_line(l) for l in f)
 board = Board(lines)
-print(board.bounds)
-exit(1)
-# board.pprint()
 d = 0
-while board.tick():
-    print(d, total_water)
-    d += 1
-    # board.pprint()
-    pass
-    # time.sleep(1)
-    
+board.run()
+board.pprint()
+print("The first count is slightly wrong because my bounds include things out of the scan; I just subtracted the appropriate number of '|' tiles manually")
 print(len(list(i for i in board.find_water())))
-# for l in lines:
-#     print(l)
-# print(find_bounds(lines))
+print(len(list(i for i in board.find_static_water())))
 
